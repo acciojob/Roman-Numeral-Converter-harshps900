2ffe700e18934e2d6b55ff9423260b45517f21c0
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-10 10:15:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+    if (num < 1 || num > 100000) return "Out of range";
 
-  //your code here
+   const numeralPairs = [
+    [1000, "M"],
+    [900,  "CM"],
+    [500,  "D"],
+    [400,  "CD"],
+    [100,  "C"],
+    [90,   "XC"],
+    [50,   "L"],
+    [40,   "XL"],
+    [10,   "X"],
+    [9,    "IX"],
+    [5,    "V"],
+    [4,    "IV"],
+    [1,    "I"]
+  ];
 
+    let result = '';
+
+    for (let [value, symbol] of numeralPairs) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+// Test cases
 
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Do not edit below this line
+module.exports = convertToRoman;
